0dbe5335cb1301ac5e596ab74b7bab36bf4b0487
sananasir236 <62425941+sananasir236@users.noreply.github.com>
2020-04-21 01:33:02 -0700

    Add files via upload

---
+++ 1.Multiplication Table.cpp.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int number,a;
+	cout<<"Enter number to find multiplication number: ";
+	cin>>number;
+	for(a=1;a<=10;a++)
+	cout<<number<<"*"<<a<<"="<<number*a<<endl;
+}
